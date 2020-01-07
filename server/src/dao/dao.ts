126b84a4b165b8c57481959a255c63a4f980afee
William Dalheim <williad@stud.ntnu.no>
2020-01-07 10:31:38 +0100

    Added parent Dao-class

---
+++ server/src/dao/dao.ts
@@ -0,0 +1,27 @@
+module.exports = class Dao{
+    pool;
+    constructor(pool){
+        this.pool = pool;
+    }
+    query(sql: string, params: any[], callback: Function) {
+        this.pool.getConnection((err, connection) => {
+          console.log('dao: trying to connect');
+          if (err) {
+            console.log('dao: error connecting');
+            callback(500, { error: 'error when connecting' });
+          } else {
+            console.log('dao: running sql: ' + sql);
+            connection.query(sql, params, (err, rows) => {
+              connection.release();
+              if (err) {
+                console.log(err);
+                callback(500, { error: 'error querying' });
+              } else {
+                console.log('dao: returning rows');
+                callback(200, rows);
+              }
+            });
+          }
+        });
+      }
+};
