b257b8e94332c9d4bd14a6de9233d069ce270054
Mahmoud Ibrahim <mahmouim@stud.ntnu.no>
2020-01-07 13:07:03 +0100

    Master merged to dao

---
+++ server/src/routes/events.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+//import userDAO
+
+const router = express.Router();
+
+// Routes to interact with events.
+
+// Get all events
+router.get("/:id", async (request, response) => {
+
+})
+// Get all
+router.get("/", async (request, response) => {
+
+})
