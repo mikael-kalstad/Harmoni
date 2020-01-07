6f836214288a744b2299484dab16b3c6e500324c
lasseis <lasse_0407@hotmail.com>
2020-01-07 12:52:18 +0100

    #20, #21, template for routes

--- server/src/hashing.ts
+++ server/src/hashing.ts
@@ -4,7 +4,7 @@ export function hash(data: string) {
     var salt = sjcl.random.randomWords(32);
     var saltstring = sjcl.codec.base64.fromBits(salt);
     return {
-        hash: sjcl.codec.base64.fromBits(sjcl.hash.sha256.hash(data+saltstring)),
+        hash: sjcl.codec.base64.fromBits(sjcl.hash.sha256.hash(data + saltstring)),
         salt: sjcl.codec.base64.fromBits(salt)
     };
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -10,4 +10,8 @@ import { hash, compareHash } from './hashing'
 server.use(body_parser.json());
 server.use(cors());
 
+
+//Our routes
+server.use("api/v0/users", require("./routes/users"));
+
 server.listen(15016, () => console.log("Server started"));
