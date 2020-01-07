fb5a44d4b2f0c8e75e18874c4c22bc9db45c2af1
baardshe <baardshe@stud.ntnu.no>
2020-01-07 11:06:33 +0100

    added eventDao functions

---
+++ server/src/dao/Event.ts
@@ -0,0 +1,9 @@
+export interface Event {
+    eventId : number,
+    arrangedBy : number,
+    location : string,
+    fromDate : string,
+    toDate : string,
+    capacity : number,
+    status : string
+}
