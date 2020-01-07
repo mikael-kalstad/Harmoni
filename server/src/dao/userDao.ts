fbc401a263fab2282943e1732f9b0c5dec83bb5b
William Dalheim <williad@stud.ntnu.no>
2020-01-07 11:04:08 +0100

    Added some User-dao functions

---
+++ server/src/dao/userDao.ts
@@ -0,0 +1,20 @@
+const dao = require("./dao.ts");
+const User = require("./User.ts");
+
+module.exports = class userDao extends dao{
+    constructor(pool){
+        super(pool);
+    }
+
+    getAllUsers(callback){
+        super.query("SELECT * FROM user", [], callback);
+    }
+
+    getUser(userId: number, callback){
+        super.query("SELECT * FROM user WHERE user_id = ?", [userId], callback);
+    }
+
+    getOrganizers(callback){
+        super.query("SELECT * FROM user WHERE type = 'organizer'")
+    }
+}
