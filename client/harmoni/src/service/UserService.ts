d2a16ae36c2a4d0cb910f6ef81064582b2cdaa62
William Dalheim <williad@stud.ntnu.no>
2020-01-07 14:52:17 +0100

    Added Service-parent

--- client/harmoni/src/service/UserService.ts
+++ client/harmoni/src/service/UserService.ts
@@ -1,3 +1,8 @@
-export default class UserService {
+import axios from "axios";
+import Service from "./Service";
 
+export default class UserService extends Service {
+    getUserById(){
+        
+    }
 }
