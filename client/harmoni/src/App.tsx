0e1b94b677edc013358d55a6e167dfb9cc3c006e
Henrik T <henrikwt@stud.ntnu.no>
2020-01-08 14:08:15 +0100

    kl1406update

--- client/harmoni/src/App.tsx
+++ client/harmoni/src/App.tsx
@@ -8,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Pages
 import FrontPage from './Components/Pages/frontPage';
 import Register from './Components/Pages/register';
+import Profile from './Components/Pages/profile';
 
 const Overlay = styled.div` 
   position: fixed;
@@ -27,6 +28,7 @@ const App: React.FC = () => {
       <Switch>
         <Route exact path='/' component={FrontPage}/>
         <Route exact path='/register' component={Register}/>
+        <Route exact path='/profile' component={Profile}/>
       </Switch>
     </Router>
   );
---
+++ client/harmoni/src/Components/Pages/profile.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Layout from '../layout';
+import styled from 'styled-components';
+
+import ProfilePageImage from '../Profile/profilePageImage';
+import ProfileOptions from '../Profile/profileOptions';
+import ArrangementGrid from '../arrangementGrid';
+
+let data = [
+    {
+        'id': 1234,
+        'category': 'Konsert',
+        'title': 'Kurt Nilsen synger låter',
+        'img': '/icons/test.jpg'
+    },
+    {
+        'id': 1234,
+        'category': 'Konsert',
+        'title': 'Kurt Nilsen synger låter',
+        'img': '/icons/test.jpg'
+    },
+    {
+        'id': 1234,
+        'category': 'Konsert',
+        'title': 'Kurt Nilsen synger låter',
+        'img': '/icons/test.jpg'
+    },
+    {
+        'id': 1234,
+        'category': 'Konsert',
+        'title': 'Kurt Nilsen synger låter',
+        'img': '/icons/test.jpg'
+    },
+    {
+        'id': 1234,
+        'category': 'Konsert',
+        'title': 'Kurt Nilsen synger låter',
+        'img': '/icons/test.jpg'
+    }
+]
+
+const Wrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    @media screen and (max-width: 700px;){
+        grid-template-columns: 1fr;
+    }
+
+`
+
+const Profile = (props:any) => (
+    <Layout>
+        <Wrapper>
+            <ProfilePageImage img='/icons/test.jpg' name='Jahn Teigen'/>
+            <ProfileOptions img='/icons/test.jpg' name='Jahn Teigen'/>
+            <ArrangementGrid data={data} title='Populære arrangementer'/>
+        </Wrapper>
+    </Layout>
+);
+//Test
+export default Profile;
