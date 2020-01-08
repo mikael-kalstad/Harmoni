8941039bd0667e049763d931137b59efc9dea963
mikaek <mikaek@ntnu.stud.no>
2020-01-08 13:17:30 +0100

    Added 404 page not found

--- client/harmoni/src/Components/Nav/navigationBar.tsx
+++ client/harmoni/src/Components/Nav/navigationBar.tsx
@@ -1,30 +1,52 @@
 import React from 'react'; 
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from '../Button/button';
 import LoginBtn from '../Button/loginBtn';
-import SmallProfileNav from '../Profile/smallProfileNav';
+import SmallProfileNav from './smallProfileNav';
+
+const StyledLink = styled(props => <Link {...props} />)`
+    color: #7f7f7f;
+    text-decoration: none;
+
+    :visited {
+        color: #7f7f7f;
+    }
+
+    :hover {
+        filter: brightness(70%);
+        text-decoration: none;
+    }
+
+    margin-left: 10px;
+`;
 
 const NavigationBar = (props:any) => (
       <Navbar bg="white" expand="lg" sticky='top' style={{boxShadow: '0px 1px 8px rgba(0, 0, 0, 0.25)'}}>
-        <Navbar.Brand href="#home">
-          <img
-              src="/icons/icon.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-              style={{margin: '25px'}}
-            />
+        <Navbar.Brand>
+          <Link to='/'>
+            <img
+                src="/icons/icon.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+                style={{margin: '25px'}}
+                onClick={() => handleIconClick('/')}
+              />
+            </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Konsert</Nav.Link>
-            <Nav.Link href="#link">Teater</Nav.Link>
-            <Nav.Link href="#link">Standup</Nav.Link>
+
+          <StyledLink to='/konsert'>Konsert</StyledLink>
+          <StyledLink to='/teater'>Teater</StyledLink>
+          <StyledLink to='/Standup'>Standup</StyledLink>
+
             <NavDropdown title="Annet" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -48,5 +70,13 @@ const NavigationBar = (props:any) => (
         </Navbar.Collapse>
     </Navbar>
 );
+
+const handleIconClick = (path: any) => {  
+  //Smooth scroll to top if already on the home page
+  if (window.location.pathname === path)
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+  else 
+      window.scrollTo(0, 0);
+}
 
 export default NavigationBar;
