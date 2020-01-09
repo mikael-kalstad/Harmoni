4de4f407d7480c21be9a7b572a2365e65fd6e81a
msandn <msandn@stud.ntnu.no>
2020-01-09 08:35:39 +0100

    Small changes to TicketService

--- client/harmoni/src/services/TicketService.ts
+++ client/harmoni/src/services/TicketService.ts
@@ -11,20 +11,20 @@ interface Ticket{
 export default class TicketService extends Service {
 
     getAllTickets(){
-        return axios.get<Ticket[]>(this.path + "/ticket").then(response => response.data);
+        return axios.get<Ticket[]>(this.path + "/tickets").then(response => response.data);
     }
 
     getAllTicketsByEventId(eventId: number){
-        return axios.get<Ticket[]>(this.path + "/ticket/event/" + eventId).then(response => response.data);
+        return axios.get<Ticket[]>(this.path + "/tickets/event/" + eventId).then(response => response.data);
     }
    
     addTickets(){
-        return axios.post(this.path + "/ticket/").then(response => response.data);
+        return axios.post(this.path + "/tickets").then(response => response.data);
     }
     deleteTicket(ticketId: number){
-        return axios.delete(this.path + "/ticket/" + ticketId).then(response => response.data);
+        return axios.delete(this.path + "/tickets/" + ticketId).then(response => response.data);
     }
     deleteTicketsByEventId(eventId: number){
-        return axios.delete(this.path + "/ticket/event" + eventId).then(response => response.data);
+        return axios.delete(this.path + "/tickets/event/" + eventId).then(response => response.data);
     }
 }
