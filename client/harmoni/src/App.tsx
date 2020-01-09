89e2ce0db4e88497c899294e814e86faf33a396f
mikaek <mikaek@ntnu.stud.no>
2020-01-09 11:36:10 +0100

    Added event data state to APP

--- client/harmoni/src/App.tsx
+++ client/harmoni/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import styled from 'styled-components';
+import { eventService } from './services/EventService';
 
 // Bootstrap styling
 import 'bootstrap/dist/css/bootstrap.min.css'; 
@@ -14,14 +14,22 @@ import PageNotFound from './Components/Pages/pageNotFound';
 import Layout from './Components/layout';
 
 const App: React.FC = () => {
+  const [userData, setUserData] = useState(undefined);
+  const [eventData, setEventData] = useState(undefined);
+
+  // Get data when component mounts
+  useEffect(() => {
+    setEventData(eventService.getAllEvents());
+  }, []);
+
   return (
     <Router>
       <Layout>
         <Switch>
-            <Route exact path='/' component={FrontPage}/>
+            <Route exact path='/' component={FrontPage} data={eventData}/>
             <Route exact path='/registrer' component={Register}/>
             <Route exact path='/profile' component={Profile}/>
-            <Route path='/event/:id' component={Event}/>
+            <Route path='/event/:id' component={Event} data={eventData}/>
             <Route component={PageNotFound} />
         </Switch>
       </Layout>
