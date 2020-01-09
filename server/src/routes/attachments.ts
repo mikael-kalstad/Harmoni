e0c2c579b311a519174b3b0a04ac4936c88286fd
Mahmoud Ibrahim <mahmouim@stud.ntnu.no>
2020-01-09 10:16:35 +0100

    Added more functions in attachmentDao,route and test #45

--- server/src/routes/attachments.ts
+++ server/src/routes/attachments.ts
@@ -14,6 +14,13 @@ router.post("/", async (request, response) => {
     });
 })
 
+// Add user to attachment in attachment_user table i DB
+router.post("/attachment_user/:attachmentId/:userId", async (request, response) => {
+    dao.addUserForAttachment(request.body, request.body, (status, data) => {
+        status == 500 ? response.status(500) : response.send(data)
+    });
+})
+
 // Get all attachments for event given eventId
 router.get("/event/:id", async (request, response) => {
     dao.getAttachmentsForEvent(parseInt(request.params.id), (status, data) => {
@@ -45,6 +52,12 @@ router.put("/:id", async (request, response) => {
 // Delete attachment given id
 router.delete("/:id", async (request, response) => {
     dao.deleteAttachment(parseInt(request.params.id), (status, data) => {
+        status == 500 ? response.status(500) : response.send(data)
+    });
+})
+// Delete attachment for user given user and attachment id
+router.delete("/attachment_user/:attachmentId/:userId", async (request, response) => {
+    dao.deleteAttachmentForUser(parseInt(request.params.id), parseInt(request.params.id),(status, data) => {
         status == 500 ? response.status(500) : response.send(data)
     });
 })
--- server/src/tests/attachment.test.ts
+++ server/src/tests/attachment.test.ts
@@ -56,3 +56,51 @@ test("Get all attachments by eventId", done => {
         done();
     })
 })
+
+test("Get all attachments by userId", done => {
+    dao.getAttachmentsForUser(2, (status, data) => {
+        expect(status).toBe(200);
+        expect(data[0].attachment_id).toBe(2);
+        done();
+    })
+})
+
+test("Get all attachments by eventId and userID", done => {
+    dao.getAttachmentsForUserForEvent(1,2, (status, data) => {
+        expect(status).toBe(200);
+        expect(data[0].attachment_id).toBe(1);
+        done();
+    })
+})
+
+test("Update attachment", done => {
+    let input =
+        {
+            attachment_id:-1,
+            event_id: -1,
+            user_id: 2,
+            data: null
+        }
+    //Actual change
+    dao.updateAttachment(input, (status, data) => {
+        expect(status).toBe(200);
+        expect(data.affectedRows).toBe(1);
+        expect(data.changedRows).toBe(1);
+
+        //No change
+        dao.updateAttachment( input, (status, data) => {
+            expect(status).toBe(200);
+            expect(data.affectedRows).toBe(1);
+            expect(data.changedRows).toBe(1);
+            done();
+        })
+    })
+})
+
+test("Delete an attachment", done => {
+    dao.deleteAttachment(1, (status, data) => {
+        expect(status).toBe(200);
+        expect(data.affectedRows).toBe(1);
+        done();
+    })
+})
