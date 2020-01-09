67e2a05eca3fa0f915675a05428555952a7cdd12
William Dalheim <williad@stud.ntnu.no>
2020-01-09 08:29:01 +0100

    Tried to merge

--- client/harmoni/src/Components/Nav/navigationBar.tsx
+++ client/harmoni/src/Components/Nav/navigationBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Nav from 'react-bootstrap/Nav';
@@ -9,76 +9,81 @@ import LoginBtn from '../Button/loginBtn';
 import SmallProfileNav from '../Profile/smallProfileNav';
 
 const StyledLink = styled(props => <Link {...props} />)`
+  color: #7f7f7f;
+  text-decoration: none;
+  // line-height: 96px;
+
+  :visited {
     color: #7f7f7f;
+  }
+
+  :hover {
+    filter: brightness(70%);
     text-decoration: none;
-    // line-height: 96px;
-
-    :visited { 
-        color: #7f7f7f; 
-    }
-
-    :hover {
-        filter: brightness(70%);
-        text-decoration: none;
-    }
-    margin: 0;
-    margin-right: 20px;
+  }
+  margin: 0;
+  margin-right: 20px;
 `;
 
-const NavigationBar = (props:any) => (
-      <Navbar bg="white" expand="lg" sticky='top' style={{boxShadow: '0px 1px 8px rgba(0, 0, 0, 0.25)'}}>
-        <Navbar.Brand>
-          <Link to='/'>
-            <img
-                src="/icons/icon.svg"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-                alt="React Bootstrap logo"
-                style={{margin: '25px'}}
-                onClick={() => handleIconClick('/')}
-              />
-            </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+const NavigationBar = (props: any) => (
+  <Navbar
+    bg="white"
+    expand="lg"
+    sticky="top"
+    style={{ boxShadow: '0px 1px 8px rgba(0, 0, 0, 0.25)' }}
+  >
+    <Navbar.Brand>
+      <Link to="/">
+        <img
+          src="/icons/icon.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+          style={{ margin: '25px' }}
+          onClick={() => handleIconClick('/')}
+        />
+      </Link>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <StyledLink to="/even?konsert">Konsert</StyledLink>
+        <StyledLink to="/event?teater">Teater</StyledLink>
+        <StyledLink to="/event?Standup">Standup</StyledLink>
 
-          <StyledLink to='/even?konsert'>Konsert</StyledLink>
-          <StyledLink to='/event?teater'>Teater</StyledLink>
-          <StyledLink to='/event?Standup'>Standup</StyledLink>
+        <NavDropdown title="Annet" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
 
-            <NavDropdown title="Annet" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-
-          {props.isLoggedIn 
-            ? (<>
-                <SmallProfileNav img='/icons/test.jpg' name='Jahn Teigen'/>
-                {/* TODO: ADD LOG OUT FUNCTIONALITY */}
-                <Button>Logg ut</Button>
-              </>)
-            : (<>
-                <StyledLink to='/registrer'>
-                  <Button>Reigstrer</Button></StyledLink>
-                <LoginBtn />
-              </>)
-          }
-        </Navbar.Collapse>
-    </Navbar>
+      {props.isLoggedIn ? (
+        <>
+          <SmallProfileNav img="/icons/test.jpg" name="Jahn Teigen" />
+          {/* TODO: ADD LOG OUT FUNCTIONALITY */}
+          <Button>Logg ut</Button>
+        </>
+      ) : (
+        <>
+          <StyledLink to="/registrer">
+            <Button>Reigstrer</Button>
+          </StyledLink>
+          <LoginBtn />
+        </>
+      )}
+    </Navbar.Collapse>
+  </Navbar>
 );
 
-const handleIconClick = (path: any) => {  
+const handleIconClick = (path: any) => {
   //Smooth scroll to top if already on the home page
   if (window.location.pathname === path)
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-  else 
-      window.scrollTo(0, 0);
-}
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  else window.scrollTo(0, 0);
+};
 
 export default NavigationBar;
--- client/harmoni/src/Components/Pages/register.tsx
+++ client/harmoni/src/Components/Pages/register.tsx
@@ -1,162 +1,154 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Dropdown from 'react-bootstrap/Dropdown';
 
 const Wrapper = styled.div`
-    margin: 80px auto 0 auto;
-    width: 400px;
+  margin: 80px auto 0 auto;
+  width: 400px;
 `;
 
 const Title = styled.h2`
-    font-size: 48px;
-    font-weight: 500;
-    text-align: center; 
-    margin: 50px;
+  font-size: 48px;
+  font-weight: 500;
+  text-align: center;
+  margin: 50px;
 `;
 
 const Input = styled.input`
-    display: block;
-    margin: 25px auto;
-    width: 400px;
-    height: 55px;
-    border: none;
-    background: #EFEFEF;
-    font-size: 18px;
-    text-indent: 15px;
+  display: block;
+  margin: 25px auto;
+  width: 400px;
+  height: 55px;
+  border: none;
+  background: #efefef;
+  font-size: 18px;
+  text-indent: 15px;
 
-    :hover {
-        filter: brightness(98%);
-    }
+  :hover {
+    filter: brightness(98%);
+  }
 `;
 
 const ButtonWrapper = styled.div`
-    margin-top: 70px;
+  margin-top: 70px;
 `;
 
 const Button = styled.button`
-    display: block;
-    margin: auto;
-    outline: none;
-    border: none;
-    height: 50px;
-    width: 400px;
-    background: #2A57AD;
-    color: white;
-    font-size: 16px;
-    margin-top: 70px;
-    cursor: pointer;
+  display: block;
+  margin: auto;
+  outline: none;
+  border: none;
+  height: 50px;
+  width: 400px;
+  background: #2a57ad;
+  color: white;
+  font-size: 16px;
+  margin-top: 70px;
+  cursor: pointer;
 
-    :hover {
-        filter: brightness(90%);
-    }
+  :hover {
+    filter: brightness(90%);
+  }
 
-    
-    :active {
-        filter: brightness(95%);
-    }
+  :active {
+    filter: brightness(95%);
+  }
 `;
 
 const WarningText = styled.p`
-    color: #E57652;
-    font-size: 16px;
-    font-weight: 500;
-    text-align: center;
+  color: #e57652;
+  font-size: 16px;
+  font-weight: 500;
+  text-align: center;
 `;
 
 const Register = () => {
-    const [nameInput, setNameInput] = useState('');
-    const [emailInput, setEmailInput] = useState('');
-    const [tlfInput, setTlfInput] = useState('');
-    const [passwordInput, setPasswordInput] = useState('');
-    const [type, setType] = useState('Velg type');
-    const [warningText, setWarningText] = useState(' ');
+  const [nameInput, setNameInput] = useState('');
+  const [emailInput, setEmailInput] = useState('');
+  const [tlfInput, setTlfInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+  const [type, setType] = useState('Velg type');
+  const [warningText, setWarningText] = useState(' ');
 
-    const types = ['Arrangør', 'Artist/Manager', 'Frivillig'];
+  const types = ['Arrangør', 'Artist/Manager', 'Frivillig'];
 
-    let menuItems: JSX.Element[] = [];
+  let menuItems: JSX.Element[] = [];
 
-    types.forEach(type => {
-        menuItems.push(
-            <Dropdown.Item 
-                onClick={() => setType(type)}
-            >{type}</Dropdown.Item>
-        );
-    });
-
-    // Check if enter key is clicked
-    const checkForEnterKey = (e: { key: string; } | undefined) =>  {
-        // Try to register if enter key is pressed down
-        if (e !== undefined && e.key === 'Enter') 
-            register(nameInput, emailInput, tlfInput, type);
-    }
-
-    const register = (name: string, email: string, tlf: string, type: string) => {
-        // TODO: Do something here...
-
-         // Check if inputs are empty
-        if (name === '' || email === '' || type === 'Velg type')
-            setWarningText('Please fill out all fields');
-        else 
-            setWarningText('Email or password is wrong');
-        }
-
-    return (
-        <>
-            <Title>Registrer bruker</Title>
-            <Wrapper>
-                <Dropdown>
-                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                        {type}
-                    </Dropdown.Toggle>
-
-                    <Dropdown.Menu>
-                    {menuItems}
-                    </Dropdown.Menu>
-                </Dropdown>
-
-                <Input
-                    onChange={e => setNameInput(e.target.value)}
-                    value={nameInput}
-                    placeholder='Navn'
-                    onKeyDown={e => checkForEnterKey(e)}
-                />
-
-                <Input 
-                    type='email'
-                    onChange={e => setEmailInput(e.target.value)}
-                    value={emailInput}
-                    placeholder='Email'
-                    onKeyDown={e => checkForEnterKey(e)}
-                />
-
-                <Input
-                    onChange={e => setTlfInput(e.target.value)}
-                    value={tlfInput}
-                    id='tlfInput'
-                    placeholder='Telefon (valgfritt)'
-                    onKeyDown={e => checkForEnterKey(e)}
-                />
-
-                <Input
-                    type='password'
-                    onChange={e => setPasswordInput(e.target.value)}
-                    value={passwordInput}
-                    placeholder='Passord'
-                    onKeyDown={e => checkForEnterKey(e)}
-                />
-
-                <WarningText>{warningText}</WarningText>
-
-                {/* <Button onClick={() => register(nameInput, emailInput, tlfInput, type)}>REGISTRER</Button> */}
-
-                <ButtonWrapper>
-                    <UploadBtn 
-                        name='REGISTRER'
-                    />
-                </ButtonWrapper>
-            </Wrapper>
-        </> 
+  types.forEach(type => {
+    menuItems.push(
+      <Dropdown.Item onClick={() => setType(type)}>{type}</Dropdown.Item>
     );
-}
+  });
+
+  // Check if enter key is clicked
+  const checkForEnterKey = (e: { key: string } | undefined) => {
+    // Try to register if enter key is pressed down
+    if (e !== undefined && e.key === 'Enter')
+      register(nameInput, emailInput, tlfInput, type);
+  };
+
+  const register = (name: string, email: string, tlf: string, type: string) => {
+    // TODO: Do something here...
+
+    // Check if inputs are empty
+    if (name === '' || email === '' || type === 'Velg type')
+      setWarningText('Please fill out all fields');
+    else setWarningText('Email or password is wrong');
+  };
+
+  return (
+    <>
+      <Title>Registrer bruker</Title>
+      <Wrapper>
+        <Dropdown>
+          <Dropdown.Toggle variant="primary" id="dropdown-basic">
+            {type}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>{menuItems}</Dropdown.Menu>
+        </Dropdown>
+
+        <Input
+          onChange={e => setNameInput(e.target.value)}
+          value={nameInput}
+          placeholder="Navn"
+          onKeyDown={e => checkForEnterKey(e)}
+        />
+
+        <Input
+          type="email"
+          onChange={e => setEmailInput(e.target.value)}
+          value={emailInput}
+          placeholder="Email"
+          onKeyDown={e => checkForEnterKey(e)}
+        />
+
+        <Input
+          onChange={e => setTlfInput(e.target.value)}
+          value={tlfInput}
+          id="tlfInput"
+          placeholder="Telefon (valgfritt)"
+          onKeyDown={e => checkForEnterKey(e)}
+        />
+
+        <Input
+          type="password"
+          onChange={e => setPasswordInput(e.target.value)}
+          value={passwordInput}
+          placeholder="Passord"
+          onKeyDown={e => checkForEnterKey(e)}
+        />
+
+        <WarningText>{warningText}</WarningText>
+
+        {/* <Button onClick={() => register(nameInput, emailInput, tlfInput, type)}>REGISTRER</Button> */}
+
+        <ButtonWrapper>
+          <UploadBtn name="REGISTRER" />
+        </ButtonWrapper>
+      </Wrapper>
+    </>
+  );
+};
 
 export default Register;
