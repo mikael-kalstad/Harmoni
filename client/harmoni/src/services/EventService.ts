741335112daf064c65a9e3f5c805192941633f1d
William Dalheim <williad@stud.ntnu.no>
2020-01-09 11:37:46 +0100

    Adjusted service classes to export instance

--- client/harmoni/src/services/EventService.ts
+++ client/harmoni/src/services/EventService.ts
@@ -1,47 +1,65 @@
-import axios from "axios";
-import Service from "./Service";
-import {renderToStaticMarkup} from "react-dom/server";
+import axios from 'axios';
+import Service from './Service';
+import { renderToStaticMarkup } from 'react-dom/server';
 interface Event {
-    eventId : number;
-    name : string;
-    organizer : number;
-    location : string;
-    fromDate : string;
-    toDate : string;
-    capacity : number;
-    status : string;
+  eventId: number;
+  name: string;
+  organizer: number;
+  location: string;
+  fromDate: string;
+  toDate: string;
+  capacity: number;
+  status: string;
 }
 
-export default class EventService extends Service {
-    getEventById(eventId: number){
-        return axios.get(this.path+'/events/'+eventId).then(response=>response.data)
-    }
+class EventService extends Service {
+  getEventById(eventId: number) {
+    return axios
+      .get(this.path + '/events/' + eventId)
+      .then(response => response.data);
+  }
 
-    getAllEvents(){
-        return axios.get(this.path+'/events/').then(response =>response.data);
-    }
+  getAllEvents() {
+    return axios.get(this.path + '/events/').then(response => response.data);
+  }
 
-    getEventsByLocation(location: string){
-        return axios.get(this.path+'/events/address/'+location).then(response =>response.data);
-    }
+  getEventsByLocation(location: string) {
+    return axios
+      .get(this.path + '/events/address/' + location)
+      .then(response => response.data);
+  }
 
-    getEventsByStatus(status: string){
-        return axios.get(this.path+'/events/status/'+status).then(respnse=>respnse.data);
-    }
-    getEventsByOrganizer(userId:number){
-        return axios.get(this.path+'/events/organizer/'+userId).then(response =>response.data);
-    }
+  getEventsByStatus(status: string) {
+    return axios
+      .get(this.path + '/events/status/' + status)
+      .then(respnse => respnse.data);
+  }
+  getEventsByOrganizer(userId: number) {
+    return axios
+      .get(this.path + '/events/organizer/' + userId)
+      .then(response => response.data);
+  }
 
-    addEvent(event:Event){
-        return axios.post(this.path+'/events/',event).then(response=>response.data);
-    }
-    updateEvent(event:Event){
-        return axios.put(this.path+'/events/'+event.eventId,event).then(response=>response.data);
-    }
-    getEventsByUser(userId:number){
-        return axios.get(this.path+'/events/user/'+userId).then(response=>response.data);
-    }
-    deleteEventById(eventId: number){
-        return axios.delete(this.path + '/events/'+eventId).then(response=>response.data);
-    }
+  addEvent(event: Event) {
+    return axios
+      .post(this.path + '/events/', event)
+      .then(response => response.data);
+  }
+  updateEvent(event: Event) {
+    return axios
+      .put(this.path + '/events/' + event.eventId, event)
+      .then(response => response.data);
+  }
+  getEventsByUser(userId: number) {
+    return axios
+      .get(this.path + '/events/user/' + userId)
+      .then(response => response.data);
+  }
+  deleteEventById(eventId: number) {
+    return axios
+      .delete(this.path + '/events/' + eventId)
+      .then(response => response.data);
+  }
 }
+
+export let eventService = new EventService();
--- client/harmoni/src/services/TicketService.ts
+++ client/harmoni/src/services/TicketService.ts
@@ -1,30 +1,39 @@
-import axios from "axios";
-import Service from "./Service";
+import axios from 'axios';
+import Service from './Service';
 
-interface Ticket{
-    ticketId: number;
-    eventId: number;
-    price: number;
-    type: string;
+interface Ticket {
+  ticketId: number;
+  eventId: number;
+  price: number;
+  type: string;
 }
 
-export default class TicketService extends Service {
+class TicketService extends Service {
+  getAllTickets() {
+    return axios
+      .get<Ticket[]>(this.path + '/tickets/')
+      .then(response => response.data);
+  }
 
-    getAllTickets(){
-        return axios.get<Ticket[]>(this.path + "/tickets/").then(response => response.data);
-    }
+  getAllTicketsByEventId(eventId: number) {
+    return axios
+      .get<Ticket[]>(this.path + '/tickets/event/' + eventId)
+      .then(response => response.data);
+  }
 
-    getAllTicketsByEventId(eventId: number){
-        return axios.get<Ticket[]>(this.path + "/tickets/event/" + eventId).then(response => response.data);
-    }
-   
-    addTickets(){
-        return axios.post(this.path + "/tickets/").then(response => response.data);
-    }
-    deleteTicket(ticketId: number){
-        return axios.delete(this.path + "/tickets/" + ticketId).then(response => response.data);
-    }
-    deleteTicketsByEventId(eventId: number){
-        return axios.delete(this.path + "/tickets/event/" + eventId).then(response => response.data);
-    }
+  addTickets() {
+    return axios.post(this.path + '/tickets/').then(response => response.data);
+  }
+  deleteTicket(ticketId: number) {
+    return axios
+      .delete(this.path + '/tickets/' + ticketId)
+      .then(response => response.data);
+  }
+  deleteTicketsByEventId(eventId: number) {
+    return axios
+      .delete(this.path + '/tickets/event/' + eventId)
+      .then(response => response.data);
+  }
 }
+
+export let ticketService = new TicketService();
