efce7cfd7d0554fb3a6d00b0a17228edc7d31911
lasseis <lasse_0407@hotmail.com>
2020-01-10 08:52:38 +0100

    Fix jest timeout

--- server/jest.config.js
+++ server/jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
         "^.+\\.tsx?$": "ts-jest"
     },
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    setupFilesAfterEnv: ['./setJestTimeout.js']
 }
-
-jest.setTimeout(15000)
