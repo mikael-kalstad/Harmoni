5e33ff2e42c666405337f8995c10c78de930e8e0
lasseis <lasse_0407@hotmail.com>
2020-01-13 13:01:59 +0100

    #76 change queries for eventDao to format datetime

--- server/src/dao/eventDao.ts
+++ server/src/dao/eventDao.ts
@@ -20,27 +20,51 @@ export default class eventDao extends daoParentEvent {
     }
 
     getAllEvents(callback) {
-        super.query('SELECT * FROM event', [], callback);
+        super.query("SELECT event_id, organizer, name, address, capacity, " +
+            "status, information, category, picture, " +
+            "DATE_FORMAT(to_date, \"%d.%m.%Y %H:%i\") as to_date, " +
+            "DATE_FORMAT(from_date, \"%d.%m.%Y %H:%i\") as from_date " +
+            "FROM event", [], callback);
     }
 
     getEvent(eventId: number, callback) {
-        super.query('SELECT * FROM event WHERE event_id = ?', [eventId], callback);
+        super.query("SELECT event_id, organizer, name, address, capacity, " +
+            "status, information, category, picture, " +
+            "DATE_FORMAT(to_date, \"%d.%m.%Y %H:%i\") as to_date, " +
+            "DATE_FORMAT(from_date, \"%d.%m.%Y %H:%i\") as from_date " +
+            "FROM event WHERE event_id = ?", [eventId], callback);
     }
 
     getEventsByAddress(location: string, callback) {
-        super.query('SELECT * FROM event WHERE address = ?', [location], callback);
+        super.query("SELECT event_id, organizer, name, address, capacity, " +
+            "status, information, category, picture, " +
+            "DATE_FORMAT(to_date, \"%d.%m.%Y %H:%i\") as to_date, " +
+            "DATE_FORMAT(from_date, \"%d.%m.%Y %H:%i\") as from_date " +
+            "FROM event WHERE address = ?", [location], callback);
     }
 
     getEventsByCapacity(capacity: number, callback) {
-        super.query('SELECT * FROM event WHERE capacity = ?', [capacity], callback);
+        super.query("SELECT event_id, organizer, name, address, capacity, " +
+            "status, information, category, picture, " +
+            "DATE_FORMAT(to_date, \"%d.%m.%Y %H:%i\") as to_date, " +
+            "DATE_FORMAT(from_date, \"%d.%m.%Y %H:%i\") as from_date " +
+            "FROM event WHERE capacity = ?", [capacity], callback);
     }
 
     getEventsByOrganizer(organizer: number, callback) {
-        super.query('SELECT * FROM event WHERE organizer = ?', [organizer], callback);
+        super.query("SELECT event_id, organizer, name, address, capacity, " +
+            "status, information, category, picture, " +
+            "DATE_FORMAT(to_date, \"%d.%m.%Y %H:%i\") as to_date, " +
+            "DATE_FORMAT(from_date, \"%d.%m.%Y %H:%i\") as from_date " +
+            "FROM event WHERE organizer = ?", [organizer], callback);
     }
 
     getEventsByStatus(status: string, callback) {
-        super.query('SELECT * FROM event WHERE status = ?', [status], callback);
+        super.query("SELECT event_id, organizer, name, address, capacity, " +
+            "status, information, category, picture, " +
+            "DATE_FORMAT(to_date, \"%d.%m.%Y %H:%i\") as to_date, " +
+            "DATE_FORMAT(from_date, \"%d.%m.%Y %H:%i\") as from_date " +
+            "FROM event WHERE status = ?", [status], callback);
     }
 
     addEvent(event: event, callback) {
@@ -56,6 +80,10 @@ export default class eventDao extends daoParentEvent {
     }
 
     getEventsOfUser(userId: number, callback) {
-        super.query('SELECT event.* FROM event, user_event WHERE user_event.user_id = ? AND event.event_id = user_event.event_id', [userId], callback)
+        super.query("SELECT event.event_id, event.organizer, event.name, event.address, event.capacity, " +
+            "event.status, event.information, event.category, event.picture, " +
+            "DATE_FORMAT(to_date, \"%d.%m.%Y %H:%i\") as to_date, " +
+            "DATE_FORMAT(from_date, \"%d.%m.%Y %H:%i\") as from_date " +
+            "FROM event" + ", user_event WHERE user_event.user_id = ? AND event.event_id = user_event.event_id", [userId], callback)
     }
 };
