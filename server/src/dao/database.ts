ad9c873e31f3d54d8b37b90a3d237b3a4e0752ce
lasseis <lasse_0407@hotmail.com>
2020-01-13 08:32:02 +0100

    Change database connection pool to read settings from a .env file

--- server/src/dao/database.ts
+++ server/src/dao/database.ts
@@ -3,10 +3,10 @@ var mysql = require("mysql");
 export const pool = mysql.createPool(
     {
         connectionLimit: 30,
-        host: "mysql.stud.iie.ntnu.no",
-        user: "mahmouim",
-        password: "7jddp0FT",
-        database: "mahmouim",
+        host: process.env.db_host,
+        user: process.env.db_user,
+        password: process.env.db_password,
+        database: process.env.db_database,
         debug: false,
         multipleStatements: true
     }
