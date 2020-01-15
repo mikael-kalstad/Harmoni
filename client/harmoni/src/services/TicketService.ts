f04e2fa4b6f37bf74f813750e4147edefd9441dc
moham <mnnayef@stud.ntnu.no>
2020-01-15 09:05:51 +0100

    send token in service files

--- client/harmoni/src/services/TicketService.ts
+++ client/harmoni/src/services/TicketService.ts
@@ -11,23 +11,35 @@ interface Ticket {
 
 class TicketService extends Service {
   getAllTickets() {
+    const headers = {
+      "Content-Type": "application/json; charset=utf-8",
+      "harmoni-token": localStorage.getItem("harmoni-token")
+    };
     updateToken();
     return axios
-      .get<Ticket[]>(this.path + '/tickets/')
+      .get<Ticket[]>(this.path + '/tickets/', {headers:headers})
       .then(response => response.data);
   }
 
   getAllTicketsByEventId(eventId: number) {
+    const headers = {
+      "Content-Type": "application/json; charset=utf-8",
+      "harmoni-token": localStorage.getItem("harmoni-token")
+    };
     updateToken();
     return axios
-      .get<Ticket[]>(this.path + '/tickets/event/' + eventId)
+      .get<Ticket[]>(this.path + '/tickets/event/' + eventId, {headers:headers})
       .then(response => response.data);
   }
 
   addTickets() {
+    const headers = {
+      "Content-Type": "application/json; charset=utf-8",
+      "harmoni-token": localStorage.getItem("harmoni-token")
+    };
     updateToken();
     return axios
-      .post(this.path + '/authorized/tickets/')
+      .post(this.path + '/authorized/tickets/', {headers:headers})
       .then(response => response.data);
   }
 
@@ -40,15 +52,23 @@ class TicketService extends Service {
   }
 
   deleteTicket(ticketId: number) {
+    const headers = {
+      "Content-Type": "application/json; charset=utf-8",
+      "harmoni-token": localStorage.getItem("harmoni-token")
+    };
     updateToken();
     return axios
-      .delete(this.path + '/authorized/tickets/' + ticketId)
+      .delete(this.path + '/authorized/tickets/' + ticketId, {headers:headers})
       .then(response => response.data);
   }
   deleteTicketsByEventId(eventId: number) {
+    const headers = {
+      "Content-Type": "application/json; charset=utf-8",
+      "harmoni-token": localStorage.getItem("harmoni-token")
+    };
     updateToken();
     return axios
-      .delete(this.path + '/authorized/tickets/event/' + eventId)
+      .delete(this.path + '/authorized/tickets/event/' + eventId, {headers:headers})
       .then(response => response.data);
   }
 }
