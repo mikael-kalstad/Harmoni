6c0196061c934bd96ea20f172e258dd86b871461
moham <mnnayef@stud.ntnu.no>
2020-01-16 15:14:31 +0100

    maps with response

--- server/src/routes/geo.ts
+++ server/src/routes/geo.ts
@@ -4,17 +4,22 @@ const router = express.Router();
 
 let options = {
   provider: 'google',
-  httpAdapter: 'http',
+  httpAdapter: 'https',
   apiKey: process.env.api_key,
   formatter: null
 };
 
 let geo = NodeGeoCoder(options);
 
-router.get('/geo/:address', async (request, response) => {
-  geo.geocode(request.params.address, (status, data) => {
-    status == 500 ? response.status(500) : response.send(data);
-  });
+router.get('/geo/', async (request, response) => {
+  var address = request.headers["address"];
+  console.log(address);
+  geo.geocode(address).then(function(res) {
+    console.log(res);
+  })
+      .catch(function(err) {
+        console.log(err);
+      });;
 });
 
 module.exports = router;
