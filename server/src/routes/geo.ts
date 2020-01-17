655d9751a836506bafa5b75165b787737ac79b4b
William Dalheim <williad@stud.ntnu.no>
2020-01-17 10:18:01 +0100

    removed node-geocoder

--- server/src/routes/geo.ts
+++ server/src/routes/geo.ts
@@ -1,16 +1,6 @@
 import express from 'express';
-const NodeGeoCoder = require('node-geocoder');
 const router = express.Router();
 const { GetLatLngByAddress } = require('geocoder-free');
-
-let options = {
-  provider: 'locationiq',
-  httpAdapter: 'https',
-  apiKey: process.env.api_key,
-  formatter: null
-};
-
-let geo = NodeGeoCoder(options);
 
 router.get('/geo/', (request, response) => {
   var address = request.headers['address'];
